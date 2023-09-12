13b5154029af4d9735d62c1bc7212b058edb4443
LongTran <longtraz@gmail.com>
2023-09-12 23:06:40 +0700

    Init project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex flex-col p-16">
+      <div className="text-6xl">Product List</div>
+    </div>
+  );
+}
+
+export default App;
