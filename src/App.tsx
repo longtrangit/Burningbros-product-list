2ea4707aa5390be941b05d90160370ca42dfc02d
LongTran <longtraz@gmail.com>
2023-09-13 14:25:23 +0700

    Add Search Section

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import { SearchSection } from "./components";
 
 function App() {
   return (
     <div className="flex flex-col p-16">
       <div className="text-6xl">Product List</div>
+      <SearchSection />
     </div>
   );
 }
---
+++ src/components/search-section/SearchSection.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export const SearchSection = () => {
+  return (
+    <div className="flex py-7 gap-5">
+      <div>Search Product</div>
+      <input className="border" />
+    </div>
+  );
+};
